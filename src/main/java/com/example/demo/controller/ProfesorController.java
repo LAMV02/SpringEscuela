587761d0da0b2493b013d5d8c44210f5cbd9a809
Luis Mendoza <luis_antonio_teemo@hotmail.com>
2025-06-05 15:36:29 -0600

    OnlySwagger Documentation
    now only you can test all the program with postman or only with the swagger documentation.

--- src/main/java/com/example/demo/controller/ProfesorController.java
+++ src/main/java/com/example/demo/controller/ProfesorController.java
@@ -2,19 +2,28 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Profesor;
 import com.example.demo.repository.ProfesorRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/Profesores")
 public class ProfesorController {
 
     @Autowired
     private ProfesorRepository profesorRepository;
 
-    public List<Profesor> getAll(){
+    @GetMapping
+    public List<Profesor> obtenertodos(){
         return profesorRepository.findAll();
+    }
+
+    @PostMapping
+    public Profesor agregar(@Valid @RequestBody Profesor profesor){
+        return profesorRepository.save(profesor);
     }
 
 }
